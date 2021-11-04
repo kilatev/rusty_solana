272e2bf01c26488c26a300789b50eb0ef54e06c9
vitalijs ozornins <vetalik@gmail.com>
2021-11-04 11:27:11 +0200

    some small refactoring, and getting to know
    with reqwest crate

--- rusty_actix/src/main.rs
+++ rusty_actix/src/main.rs
@@ -5,6 +5,16 @@ use std::collections::HashMap;
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
+#[get("/getSlot")]
+async fn getSlot() -> impl Responder {
+    let resp = request_slot();
+    if resp.is_ok() {
+        HttpResponse::Ok().body(format!("{} {:#?}", "Hey there!", resp))
+    } else {
+        print!("{:#?}", resp);
+        HttpResponse::Ok().body(format!("{} {}", "Hey there!", "aa"))
+    }
+}
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
@@ -17,9 +27,34 @@ fn opa() -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
     println!("{:#?}", resp);
     Ok(resp)
 }
+fn request_slot() -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::blocking::Client::new();
+    println!("AHAHAHHAHAHHAH");
+    let resp = client
+        .post("https://api.mainnet-beta.solana.com")
+        .body("{\"jsonrpc\":\"2.0\",\"id\":1, \"method\":\"getSlot\"}")
+        .send()?
+        .json::<HashMap<String, String>>()?;
+    let res = format!("{:#?}", resp);
+    Ok(res)
+}
 
 async fn manual_hello() -> impl Responder {
     let resp = opa();
+    match resp {
+        Ok(z) => {
+            let mut res = "";
+            match z.get("origin") {
+                Some(v) => res = v,
+                _ => res = "",
+            }
+            HttpResponse::Ok().body(format!("{} {}", "Origin!", res))
+        }
+        _ => HttpResponse::Ok().body(format!("{} {:#?}", "Error ws there!", resp)),
+    }
+}
+async fn get_slot() -> impl Responder {
+    let resp = request_slot();
     if resp.is_ok() {
         HttpResponse::Ok().body(format!("{} {:#?}", "Hey there!", resp))
     } else {
@@ -33,9 +68,10 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(getSlot)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind("127.0.0.1:8081")?
+    .bind("127.0.0.1:8082")?
     .run()
     .await
 }
